b35e0a6379c087893a8d35b7c15f43458354dbf8
Bulutze <zzeynepcagil@gmail.com>
2025-07-15 19:22:08 +0300

    updated

---
+++ Algorithms/Constructive-Algorithms/new-year-chaos.py
@@ -0,0 +1,12 @@
+def minimumBribes(q):
+    bribes=0
+    for i in range(len(q)):
+        if q[i] - (i + 1) > 2:
+            print("Too chaotic")
+            return
+       
+        for j in range(max(0, q[i] - 2), i):
+            if q[j] > q[i]:
+                bribes += 1
+    
+    print(bribes)
